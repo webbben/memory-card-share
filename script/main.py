3e798cad82774cc1d4828cb87c786e9aad3d3545
Ben Webb <ben.webb340@gmail.com>
2024-02-06 18:08:08 +0900

    Ben Webb: testing if pushing works properly

--- script/main.py
+++ script/main.py
@@ -6,11 +6,21 @@ REPO_URL = "https://github.com/webbben/memory-card-share.git"
 def get_project_root():
     return os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 
-def saveMemoryCardChanges(user: str = "UnknownUser"):
+def get_github_username(repo: git.Repo):
+    'gets the github username of the user associated with the git configuration'
+    username = repo.config_reader().get_value("user", "name")
+    if username == "" or username == None:
+        return "unknown-user"
+    return username
+
+def saveMemoryCardChanges(msg: str = ""):
     'Pushes any existing changes to local memory cards to the remote repo'
+    if msg == "":
+        msg = "Saved data to memory card in slot A"
     repo_path = get_project_root()
     repo = git.Repo(repo_path)
-    commitMessage = f"{user}: Saved data to memory card in slot A"
+    user = get_github_username(repo)
+    commitMessage = f"{user}: {msg}"
     # add, commit and push
     repo.git.add(all=True)
     repo.index.commit(commitMessage)
@@ -23,4 +33,4 @@ def loadMemoryCardData():
     repo.remotes.origin.pull()
 
 if __name__ == "__main__":
-    saveMemoryCardChanges("webbben")
+    saveMemoryCardChanges("testing if pushing works properly")
