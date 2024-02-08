75cdcb5a958a92aab49bb5c67344bbf4b4c851a0
Ben Webb <ben.webb340@gmail.com>
2024-02-08 16:07:23 +0900

    tweak windows install output

--- install/setup-windows-default.bat
+++ install/setup-windows-default.bat
@@ -8,10 +8,13 @@ git clone %repoUrl% %repoName%
 cd %repoName%
 pipenv install
 
+echo.
+echo.
 echo Repository cloned and program setup successfully!
 echo You can close this window and go to the newly created folder called ^"ZA GAMECUBE MANAGER^".
 echo In there, run the file called ^"run-windows.bat^" to start the program.
 echo.
 echo Note: If you need to run this script again to re-install from scratch, please delete the generated folder first.
 echo Not doing so could cause unexpected errors.
+echo.
 pause
