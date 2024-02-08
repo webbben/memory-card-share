60dc2bfd9873a38c8fe61a19b6c61b1dddf24182
Ben Webb <ben.webb340@gmail.com>
2024-02-08 18:53:00 +0900

    fixed windows dependency installer

--- install/install-python-windows.bat
+++ install/install-python-windows.bat
@@ -1,58 +1,52 @@
 @echo off
 
-REM Gets the python3 version if it exists
-for /f "tokens=2 delims= " %%G in ('python3 --version 2^>nul') do set "pythonVersion=%%G"
-
-REM Check if Python 3 is not installed or its version is below 3.10
-if not defined pythonVersion (
-    set "installPython=true"
-    echo "No python3 installation found."
-) else (
-    REM Extract major and minor version numbers
-    for /f "tokens=1,2 delims=." %%A in ("%pythonVersion%") do (
-        set /a majorVersion=%%A
-        set /a minorVersion=%%B
-    )
-    REM If version is below 3.10, upgrade to Python 3.12.0
-    if %majorVersion% lss 3 if %minorVersion% lss 10 (
-        set "installPython=true"
-        echo "Python version %majorVersion%.%minorVersion% found. Lets upgrade to 3.12.0!"
-    )
-)
-
-REM Install/upgrade Python if needed
-if defined installPython (
-    echo "Installing Python 3.12.0..."
+REM Install Python 3
+where python3 >nul 2>nul
+if %errorlevel% neq 0 (
+    echo "Python 3 installation not found. Installing Python 3.12.0..."
     curl -o python-installer.exe -L "https://www.python.org/ftp/python/3.12.0/python-3.12.0-amd64.exe"
     start /wait python-installer.exe
     del python-installer.exe
     echo "Python installation successful!"
+) else (
+    echo "Python 3 already installed. Nice!"
 )
 
 REM Install Pip
-if not exist "%pythonPath%\Scripts\pip.exe" (
+where pip >nul 2>nul
+if %errorlevel% neq 0 (
     echo "No Pip installation found. Installing..."
-    easy_install pip
+    python -m ensurepip --default-pip
     echo "Pip installation successful!"
+) else (
+    echo "Pip already installed. Nice!"
 )
 
-REM Install Pipenv
-if not exist "%pythonPath%\Scripts\pipenv.exe" (
+where pipenv >nul 2>nul
+if %errorlevel% neq 0 (
     echo "No Pipenv installation found. Installing..."
     pip install pipenv
     echo "Pipenv installation successful!"
+) else (
+    echo "Pipenv already installed. Nice!"
 )
 
 REM Install Git
-if not exist "%ProgramFiles%\Git" (
+where git >nul 2>nul
+if %errorlevel% neq 0 (
     echo "No Git installation found. Installing..."
     curl -o git-installer.exe -L "https://github.com/git-for-windows/git/releases/download/v2.37.0.windows.1/Git-2.37.0-64-bit.exe"
     start /wait git-installer.exe
     del git-installer.exe
     echo "Git installation successful!"
+) else (
+    echo "Git already installed. Nice!"
 )
 
 REM Display versions
+echo.
+echo "Here are the versions of the dependencies"
+echo.
 python --version
 pip --version
 pipenv --version
