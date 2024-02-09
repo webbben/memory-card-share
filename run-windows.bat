58794cb79a269bbb0f95967f2f6d653d69972f7e
Benjamin Webb <38891424+webbben@users.noreply.github.com>
2024-02-09 17:13:15 +0900

    Update run-windows.bat

--- run-windows.bat
+++ run-windows.bat
@@ -1,4 +1,11 @@
 @echo off
+
+REM go to the directory this script is running in
+cd /d "%~dp0"
+
+REM check if we pipenv needs to install new dependencies
 pipenv install --ignore-pipfile
+
+REM run the python script entry point
 cd script
 pipenv run python main.py
